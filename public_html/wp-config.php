9116c6f2ca050edbf341537ee6ff74b511d74366
cyponthemic <alex.chavet@gmail.com>
2014-06-25 11:51:20 +1000

    Update wp-config.php

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -20,22 +20,22 @@ include( dirname( __FILE__ ) . '/wp-config-local.php' );
 } else {
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'goodtale_wp345');
+define('DB_NAME', '**');
 
 /** MySQL database username */
-define('DB_USER', 'goodtale_wp345');
+define('DB_USER', '**');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'w7a)S13)9P');
+define('DB_PASSWORD', '**');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '**');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', '**');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', '**');
 }
 /**#@+
  * Authentication Unique Keys and Salts.
