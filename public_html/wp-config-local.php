68b8ceeb5200abb11853fad461a722b85ea2c1db
Alex <Chavet>
2014-06-25 15:06:02 +1000

    I made my changes for local database

--- public_html/wp-config-local.php
+++ public_html/wp-config-local.php
@@ -17,13 +17,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_goodtalent');
+define('DB_NAME', 'goodtale_wp345');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'localusing');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'localuser');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
