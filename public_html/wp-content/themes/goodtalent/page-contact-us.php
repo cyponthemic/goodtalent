6cf0a27e746f16b5ac030939d250f22a372d6f5c
Alex <Chavet>
2014-06-26 20:39:23 +1000

    Contact responsive V2
    Ajusting the <aside>

--- public_html/wp-content/themes/goodtalent/page-contact-us.php
+++ public_html/wp-content/themes/goodtalent/page-contact-us.php
@@ -29,7 +29,7 @@ get_header(); ?>
 
 		</div>
 	</div>
-	<aside id="side_form" class="Right large-6 medium-5 large-offset-0 medium-offset-0 small-10 small-offset-1 columns left">
+	<aside id="side_form" class="Right large-6 medium-5 large-offset-0 medium-offset-0 small-10 small-offset-0 columns left">
 	<?php echo do_shortcode( '[contact-form-7 id="1234" title="Contact form 1"]' ); ?>
 	</aside>
 </div>
